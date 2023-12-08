7df9fba11ba2eb6bc318d8bd404687a327bd7b5b
Artur Fiorentino <afiorentino1415@gmail.com>
2023-12-07 22:26:21 -0300

    DesktopPR finalizado

---
+++ Introducao/DesktopPR/Components/PessoaControl.cs
@@ -0,0 +1,61 @@
+﻿using DesktopPR.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopPR.Components
+{
+    public partial class PessoaControl : UserControl
+    {
+        public PessoaControl()
+        {
+            InitializeComponent();
+        }
+
+        public PessoaControl(Pessoa pessoa)
+        {
+
+            InitializeComponent();
+            Pessoa = pessoa;
+            // Rotação da imagem
+            Image imagemRotacionada = RotacionarImagem(Image.FromStream(new MemoryStream(pessoa.Foto)));
+
+            pictureBox1.Image = imagemRotacionada;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+            label1.Text = pessoa.Nome;
+            label2.Text = pessoa.CPF;
+            label3.Text = pessoa.DataNascimento.Value.ToShortDateString();
+        }
+
+        public Pessoa Pessoa { get; }
+
+        private Image RotacionarImagem(Image imagem)
+        {
+            // Verificar a orientação da imagem e rotacionar conforme necessário
+            if (Array.IndexOf(imagem.PropertyIdList, 274) > -1)
+            {
+                var orientation = (int)imagem.GetPropertyItem(274).Value[0];
+                if (orientation == 6)
+                    imagem.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                else if (orientation == 8)
+                    imagem.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                else if (orientation == 3)
+                    imagem.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            }
+
+            return imagem;
+        }
+        private void PessoaControl_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Introducao/DesktopPR/Form1.cs
@@ -0,0 +1,50 @@
+﻿using DesktopPR.Components;
+using DesktopPR.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopPR
+{
+    public partial class Form1 : Form
+    {
+        DesktopPREntities ctx = new DesktopPREntities();
+        public Form1()
+        {
+            InitializeComponent();
+            dt.Columns.Add("Nome");
+            dt.Columns.Add("CPF");
+            dt.Columns.Add("Genero");
+            dt.Columns.Add("Telefone");
+            dt.Columns.Add("Idade");
+        }
+        DataTable dt = new DataTable();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                var lista = ctx.Pessoa.ToList();
+
+            foreach (var pessoa in lista)
+            {
+                dt.Rows.Add(pessoa.Nome, pessoa.CPF, pessoa.Sexo, pessoa.Telefone, Math.Truncate(DateTime.Now.Subtract(pessoa.DataNascimento.Value).TotalDays / 365));
+
+                PessoaControl p = new PessoaControl(pessoa);
+
+                flowLayoutPanel1.Controls.Add(p);
+            }
+
+            //Configuração DataGridView
+            dataGridView1.DataSource = dt;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.ReadOnly = true;
+
+            //Confiuração do FlowLayout
+            flowLayoutPanel1.AutoScroll = true;
+        }
+    }
+}
