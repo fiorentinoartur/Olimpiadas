ac70065ec6b3071fc942d8f74f72fe1b9125851d
Artur Fiorentino <artur.alunosenai@gmail.com>
2024-01-18 12:47:24 -0300

    post login finalizado

--- MobileMG - Nacional 2020/App1/App1/WsTowerFlyoutDetail.xaml.cs
+++ MobileMG - Nacional 2020/App1/App1/WsTowerFlyoutDetail.xaml.cs
@@ -19,7 +19,22 @@ namespace App1
 
         private void AbrirModal(object sender, EventArgs e)
         {
-            Navigation.PushModalAsync(new ModalInformation());
+            var modal = new ModalInformation();
+
+          
+            var absoluteLayout = new AbsoluteLayout();
+
+            
+            AbsoluteLayout.SetLayoutFlags(modal, AbsoluteLayoutFlags.All);
+            AbsoluteLayout.SetLayoutBounds(modal, new Rectangle(0.5, 0.5, 0.8, 0.8));
+
+           
+
+   
+            modal.Content = absoluteLayout;
+
+            Navigation.PushModalAsync(modal);
         }
+
     }
 }
